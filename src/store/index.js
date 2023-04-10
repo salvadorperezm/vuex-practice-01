c6db9be98e2823125580d3df86d2f3e444ab8f3d
salvadorperezm <salvadorperezm115@gmail.com>
2023-04-10 16:21:42 -0600

    added products store module

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex';
 
 import auth from './modules/auth';
+import products from './modules/products';
 
 export const store = createStore({
   modules: {
     auth,
+    products,
   },
 });
